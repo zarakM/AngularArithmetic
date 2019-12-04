816d52fac62a019ec4f47052e1bb2716b756144d
zarakM <mughalzarak@gmail.com>
2019-12-03 22:50:25 -0800

    no sign after one arithmetic done

--- src/app/arithmetic/arithmetic.component.ts
+++ src/app/arithmetic/arithmetic.component.ts
@@ -13,35 +13,67 @@ import * as _ from 'lodash';
 
 export class ArithmeticComponent implements OnInit {
   constructor(private http: HttpClient) { }
-  ngOnInit() {    
+  ngOnInit() {
 
-    let courses = this.http.get<any>("http://localhost:54405/api/plus/10/9").subscribe(data=>{
+    let courses = this.http.get<any>("http://localhost:54405/api/plus/10/9").subscribe(data => {
       console.log(data)
     })
   }
 
   add() {
-    let courses = this.http.get<any>("http://localhost:54405/api/plus/4/9").subscribe(data=>{
+    let courses = this.http.get<any>("http://localhost:54405/api/plus/4/90").subscribe(data => {
       console.log(data)
     })
   }
 
   subtract() {
-    let courses = this.http.get<any>("http://localhost:54405/api/minus/4/9").subscribe(data=>{
+    let courses = this.http.get<any>("http://localhost:54405/api/minus/4/9").subscribe(data => {
       console.log(data)
     })
   }
 
   multiply() {
-    let courses = this.http.get<any>("http://localhost:54405/api/multiply/4/9").subscribe(data=>{
+    let courses = this.http.get<any>("http://localhost:54405/api/multiply/4/9").subscribe(data => {
       console.log(data)
     })
   }
 
   divide() {
-    let courses = this.http.get<any>("http://localhost:54405/api/divide/4/9").subscribe(data=>{
+    let courses = this.http.get<any>("http://localhost:54405/api/divide/4/9").subscribe(data => {
       console.log(data)
     })
   }
 
+  result() {
+    document.getElementById("calcResult").innerHTML = ""
+  }
+
+  clear() {
+    document.getElementById("calcText").innerHTML = ""
+  }
+
+  pressed(key: any) {
+    let keys = String(key)
+    let already_text = document.getElementById("calcText").innerHTML
+    let length = already_text.length
+
+    if (already_text === "") {
+      if (keys === "/" || keys == "-" || keys == "+" || keys == "x") {
+
+      } else {
+        document.getElementById("calcText").innerHTML += key
+      }
+    } else {
+      let last_alpha = already_text[length - 1]
+      if (last_alpha === "/" || last_alpha == "-" || last_alpha == "+" || last_alpha == "x") {
+        if (keys === "/" || keys == "-" || keys == "+" || keys == "x") {
+
+        } else {
+          document.getElementById("calcText").innerHTML += keys
+        }
+      } else {
+        document.getElementById("calcText").innerHTML += keys
+      }
+    }
+  }
 }
